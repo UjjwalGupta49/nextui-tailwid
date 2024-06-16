f8c1b0952e9b95d417786f49e764d8ba412bf087
unknown <ujjwalg056@gmail.com>
2024-06-16 17:56:50 +0530

    initialized template

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from "tailwindcss";
+const { nextui } = require('@nextui-org/react');
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        blossomPink: '#FFC0CB',
+        lightGreen: '#90EE90',
+        skyBlue: '#87CEEB',
+        creamWhite: '#FFFDD0',
+        earthBrown: '#A0522D',
       },
     },
   },
-  plugins: [],
+  // darkMode: 'class',
+  plugins: [nextui()],
 };
-export default config;
